d9c25fc25b7e111c462baef393ee116b7b0aa436
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-08-28 20:27:50 -0400

    Organizing elements
    A transaction object with properties or a category object with an array
    of transaction objects inside? Deciding…

---
+++ script.js
@@ -0,0 +1,15 @@
+// var transaction = {
+//   info: 'Nissan gas @ $3.13',
+//   price: 32.72,
+//   date: 20180828,
+//   displayTransaction: function () {
+//     console.log(this);
+//   }
+// };
+
+
+// OR 
+
+var category = {
+  transactions: [] // later, add objects to this array
+};
