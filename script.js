45d17e61ff8b35b8671e9e0d92b2289c45699316
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-08-29 07:44:58 -0400

    displayTransactions and addTransaction methods
    Mostly just basing this off of how the todo list functions in Watch and
    Code for now

--- script.js
+++ script.js
@@ -11,5 +11,32 @@
 // OR 
 
 var category = {
-  transactions: [] // later, add objects to this array
+  transactions: [], // later, add objects to this array
+  
+  displayTransactions: function () {
+    // print transaction information to the console
+    if (this.transactions.length > 0) {
+      console.log('My Transactions:');
+      for (var i = 0; i < this.transactions.length; i++) {
+        console.log('Transaction',
+                    i,
+                    '-',
+                    this.transactions[i].info,
+                    this.transactions[i].price,
+                    this.transactions[i].date,
+                    this.transactions[i].note);
+      } // end for loop
+    } else { // end if statement
+      console.log('You don\'t have any transactions');
+    } // end else statement
+  }, // end displayTransaction method
+  
+  addTransaction: function (info, price, date, note = '') {
+    this.transactions.push({
+      info: info,
+      price: price,
+      date: date,
+      note: note
+    });
+  } // end addTransaction method
 };
